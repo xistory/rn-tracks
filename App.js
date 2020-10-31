7b687704b3d5454cc4c7e7efc585aae0603b7090
xistory <ximinseo@hotmail.com>
2020-10-31 10:39:03 +0900

    Tracking app

--- App.js
+++ App.js
@@ -1,21 +1,49 @@
-import { StatusBar } from 'expo-status-bar';
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import {
+    createAppContainer,
+    createSwitchNavigator
+} from 'react-navigation';
+import { createStackNavigator } from 'react-navigation-stack';
+import { createBottomTabNavigator } from 'react-navigation-tabs';
+import { setNavigator } from './src/navigationRef';
 
-export default function App() {
-  return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
-  );
-}
+import AccountScreen from './src/screens/AccountScreen';
+import SigninScreen from './src/screens/SigninScreen';
+import SignupScreen from './src/screens/SignupScreen';
+import TrackCreateScreen from './src/screens/TrackCreateScreen';
+import TrackDetailScreen from './src/screens/TrackDetailScreen';
+import TrackListScreen from './src/screens/TrackListScreen';
+import { Provider as AuthProvider } from './src/context/AuthContext';
+import { Provider as LocationProvider } from './src/context/LocationContext';
+import { Provider as TrackProvider } from './src/context/TrackContext';
+import ResolveAuthScreen from './src/screens/ResolveAuthScreen';
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
+const switchNavigator = createSwitchNavigator({
+    ResolveAuth: ResolveAuthScreen,
+    loginFlow: createStackNavigator({
+        Signup: SignupScreen,
+        Signin: SigninScreen
+    }),
+    mainFlow: createBottomTabNavigator({
+        trackListFlow: createStackNavigator({
+            TrackList: TrackListScreen,
+            TrackDetail: TrackDetailScreen
+        }),
+        CreateTrack: TrackCreateScreen,
+        Account: AccountScreen
+    })
 });
+
+const App = createAppContainer(switchNavigator);
+
+export default () => {
+    return (
+        <TrackProvider>
+        <LocationProvider>
+            <AuthProvider>
+            <App ref={(navigator) => { setNavigator(navigator) }} />
+            </AuthProvider>
+        </LocationProvider>
+        </TrackProvider>
+    );
+};
---
+++ src/_mockLocation.js
@@ -0,0 +1,26 @@
+import * as Location from 'expo-location';
+
+const tenMetersWithDegree = 0.0001;
+
+const getLocation = increment => {
+    return {
+        timestamp: 10000000,
+        coords: {
+            speed: 0,
+            accuracy: 5,
+            altitudeAccuracy: 5,
+            altitude: 5,
+            longitude: -122.0312186 + increment * tenMetersWithDegree,
+            latitude: 37.33233141 + increment * tenMetersWithDegree
+        }
+    };
+};
+
+let counter = 0;
+setInterval(() => {
+    Location.EventEmitter.emit('Expo.locationChanged', {
+        watchId: Location._getCurrentWatchId(),
+        location: getLocation(counter)
+    });
+    counter++;
+}, 1000);
